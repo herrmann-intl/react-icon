e5312df66e8a95fcd167fcc12018d2a33a474f29
Andrew Swerlick <andrew.swerlick@hbdi.com>
2016-09-09 11:36:44 -0400

    Allowing the Icon component to take an option iconComponent argument to use something besides a 'use' tag
    This is so that we can pass smart versions of the use tag in that work around limitations with
    regards to browser compatibility and printing. The iconComponent will be a react component that
    takes a xlinkHref prop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import _ from 'lodash'
+
 /*
   Adds the provided svg document to the page as an icon. The css class ".icon"
   Ensures that the element will be sized to 1em. and share the current font cssColor
@@ -11,11 +12,15 @@ export default class Icon extends React.Component {
     let style = this.props.style || {}
     style = _.merge({height: "1em", width: "1em", fontSize: this.props.fontSize})
 
+    let iconElement = <use xlinkHref={this.this.props.icon}></use>
+    if(this.props.iconComponent){
+      iconElement = React.createElement(this.props.iconComponent, {xlinkHref: this.props.xlinkHref})
+    }
 
     return (
       <span className={this.props.className}>
         <svg viewBox="0 0 100 100" width="100%" height="100%" style={style}>
-          <use xlinkHref={this.props.icon}></use>
+          {iconElement}
         </svg>
       </span>
     )
